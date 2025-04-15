15c9322ca0fd45172dcf4c2279d7354aed055d70
Sawariya-Dixit <sawariyadixit085@gmail.com>
2025-04-15 07:01:28 +0530

    Day 3 Done

---
+++ day3.java
@@ -0,0 +1,33 @@
+public class day3 {
+
+    public static int  sell(int arr[]){
+ 
+         
+         int buyStock = Integer.MAX_VALUE;
+         int  max = 0;
+
+         for (int i = 0; i < arr.length; i++) {
+             if (buyStock<arr[i]) {
+                
+                 int profit  = arr[i]- buyStock;
+                   
+                  if (max<profit) {
+                     max = profit;
+                  }
+                  
+                   
+             }
+             else{
+                buyStock = arr[i];
+             }
+           
+
+         }
+        return max;
+    }
+  public static void main(String[] args) {
+    int arr [] = {7, 1 , 5,  3, 6, 4};
+        
+   System.out.println( sell(arr));
+  }  
+}
