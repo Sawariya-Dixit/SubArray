9294f251313c7c3c0da0d83016fb3fe49c096dfd
Sawariya-Dixit <sawariyadixit085@gmail.com>
2025-04-18 06:55:34 +0530

    Day 6 Done

---
+++ day6.java
@@ -0,0 +1,26 @@
+public class day6 {
+    
+     
+        public static void printArray(int arr []){
+      
+   
+            
+           for (int i = 0; i < arr.length; i++) {
+                  int currnet = arr[i];
+                  for (int j = i+1; j   < arr.length; j++) {
+                             System.out.print("("+currnet+","+arr[j]+")");
+                  }
+                  System.out.println();
+           }
+        }
+   
+        public static void main(String[] args) {
+            
+   
+            int arr [] = {2,4,6,8,10};
+   
+            printArray(arr);
+        }
+   }
+   
+
