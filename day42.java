69397268a50e73ec77fda6f44ad53d4c62d409ac
Sawariya-Dixit <sawariyadixit085@gmail.com>
2025-05-24 08:13:34 +0530

    Day 42 down

---
+++ day42.java
@@ -0,0 +1,65 @@
+public class day42 {
+
+    public static void printArr(int arr []){
+
+        for (int j = 0; j < arr.length; j++) {
+             System.out.print(arr[j]+" ");
+        }
+    
+    }
+
+    public static void mergeSort(int arr[] , int si , int ei){
+
+        if (si >= ei ) {
+            return;
+        }
+
+         int mid = si+(ei-si)/2;
+         mergeSort(arr, si, mid);
+         mergeSort(arr, mid+1, ei);
+
+         merg(arr,si,mid,ei);
+    }
+
+    public static void merg(int arr[], int si ,int mid , int ei){
+        int temp [] = new int[ei-si+1];
+        int i = si;
+        int j = mid+1;
+        int k = 0;
+        while (i <= mid && j <= ei) {
+            
+             if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++; 
+             }
+             else{
+                temp[k] = arr[j];
+                 j++;
+             }
+             k++;
+
+        }
+
+        while (i <=mid ) {
+             temp[k++] = arr[i++];
+        }
+        while (j <=ei) {
+            
+            temp[k++] = arr[j++];
+        }
+
+        for ( k = 0, i=si; k < temp.length; k++ ,i++) {
+             arr[i] = temp[k];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6,3,9,5,2,8};
+        mergeSort(arr, 0,arr.length-1);
+        printArr(arr);
+
+    }
+}
+ 
+
