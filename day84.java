d886ae59f06f03587c8bc6ffe181b8d07ed7e942
Sawariya-Dixit <sawariyadixit085@gmail.com>
2025-07-05 07:25:08 +0530

    post order

---
+++ day84.java
@@ -0,0 +1,47 @@
+public class day84 {
+    
+     static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int[] nodes) {
+            idx++;
+            if (nodes[idx] == -1) return null;
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void PostOrder(Node root) {
+        if (root == null) return;
+
+        PostOrder(root.left);
+        PostOrder(root.right);
+        System.out.print(root.data+"  ");
+
+      
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node root = BinaryTree.buildTree(nodes);
+
+        System.out.println("post order traversal");
+        PostOrder(root);
+
+       
+    }
+}
