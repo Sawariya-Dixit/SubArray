6feb99e5ec21226035afab8177ad6e4bd26e0e5f
Sawariya-Dixit <sawariyadixit085@gmail.com>
2025-07-11 10:02:47 +0530

    top view of a tree

---
+++ day90.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class day90 {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    static class info {
+        Node node;
+        int hd;
+
+        public info(Node node, int hd) {
+            this.hd = hd;
+            this.node = node;
+        }
+    }
+
+    public static void topView(Node root) {
+        Queue<info> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0, max = 0;
+        q.add(new info(root, 0));
+
+        while (!q.isEmpty()) {
+            info curr = q.remove();
+
+            if (!map.containsKey(curr.hd)) {
+                map.put(curr.hd, curr.node);
+            }
+
+            if (curr.node.left != null) {
+                q.add(new info(curr.node.left, curr.hd - 1));
+                min = Math.min(min, curr.hd - 1);
+            }
+
+            if (curr.node.right != null) {
+                q.add(new info(curr.node.right, curr.hd + 1));
+                max = Math.max(max, curr.hd + 1);
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        topView(root);
+    }
+}
